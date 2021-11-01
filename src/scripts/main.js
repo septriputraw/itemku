800059fff6d41da73f0a718bca0bdeb8b36fc1f2
septriputraw <36685286+septriputraw@users.noreply.github.com>
2021-11-01 15:22:33 +0700

    change to async function

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,311 +2,135 @@ function main() {
 
     const baseUrl = "http://localhost:5000";
 
-    //ambil data report
-    const getTrx = () => {
-        fetch(`${baseUrl}/report`)
-         .then(response => {
-             return response.json();
-         })
-         .then(responseJson => {
-            if(responseJson.error) {
-                showResponseMessage(responseJson.message);
-            } else {
-                renderAllTransactions(responseJson.data);
-            }
-         })
-         .catch(error => {
-             showResponseMessage(error);
-         })
-    };
-
-    //ambil data item
-    const getItem = () => {
-        fetch(`${baseUrl}/item`)
-         .then(response => {
-             return response.json();
-         })
-         .then(responseJson => {
-            if(responseJson.error) {
-                showResponseMessage(responseJson.message);
-            } else {
-                renderAllItems(responseJson.data);
-            }
-         })
-         .catch(error => {
-             showResponseMessage(error);
-         })
-    };
-    //dengan await
-    /*const getBook = async () => {
+    // get item
+    const getItem = async () => {
         try {
-          const response = await fetch(`${baseUrl}/list`);
+          const response = await fetch(`${baseUrl}/item`);
           const responseJson = await response.json();
           if(responseJson.error) {
              showResponseMessage(responseJson.message);
           } else {
-             renderAllBooks(responseJson.books);
+             renderAllItems(responseJson.data);
           }
         } catch(error) {
            showResponseMessage(error);
         }
-    }*/
+    }
 
-    /*const getBook = () => {
-        // membuat instance dari XMLHttpRequest
-        const xhr = new XMLHttpRequest();
-            
-        //menetapkan callback jika response sukses dan error
-        xhr.onload = function() {
-            const responseJson = JSON.parse(this.responseText);
-            if(responseJson.error) {
-               showResponseMessage(responseJson.message);
-            } else {
-               renderAllBooks(responseJson.books);
-            }
+    // get transaction history
+    const getTrx = async () => {
+        try {
+          const response = await fetch(`${baseUrl}/report`);
+          const responseJson = await response.json();
+          if(responseJson.error) {
+             showResponseMessage(responseJson.message);
+          } else {
+             renderAllTransactions(responseJson.data);
+          }
+        } catch(error) {
+           showResponseMessage(error);
         }
-     
-        xhr.onerror = function() {
-            showResponseMessage();
+    }
+
+    // search transaction
+    const searchTrx = async (query) => {
+        try {
+          const response = await fetch(`${baseUrl}/report/search?cari=${query}`);
+          const responseJson = await response.json();
+          if(responseJson.error) {
+             showResponseMessage(responseJson.message);
+          } else {
+             renderSeacrh(responseJson.data);
+          }
+        } catch(error) {
+           showResponseMessage(error);
         }
-     
-        // Membuat GET request dan menetapkan target URL
-        xhr.open("GET", `${baseUrl}/list`);
-        // Mengirimkan request
-        xhr.send();
-    };*/
+    }
 
-    //dengan fetch
-    const insertBook = (book) => {
-        fetch(`${baseUrl}/item`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            //    "X-Auth-Token": "12345"
-            },
-            body: JSON.stringify(book)
-        })
-        .then(response => {
-            return response.json();
-        })
-        .then(responseJson => {
-            showResponseMessage(responseJson.message);
-            getItem();
-        })
-        .catch(error => {
-            showResponseMessage(error);
-        })
-    };
-
-    //dengan fetch
-    const insertTrx = (trx) => {
-        fetch(`${baseUrl}/transaction`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            //    "X-Auth-Token": "12345"
-            },
-            body: JSON.stringify(trx)
-        })
-        .then(response => {
-            return response.json();
-        })
-        .then(responseJson => {
-            showResponseMessage(responseJson.message);
-            getTrx();
-        })
-        .catch(error => {
-            showResponseMessage(error);
-        })
-    };
-
-    //dengan await/async
-    /*const insertBook = async (book) => {
+    // add item 
+    const insertItem = async (item) => {
         try{
             const options = {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
-                    "X-Auth-Token": "12345"
+                    // "X-Auth-Token": "12345"
                 },
-                body: JSON.stringify(book)
+                body: JSON.stringify(item)
             }
      
-            const response = await fetch(`${baseUrl}/add`, options)
+            const response = await fetch(`${baseUrl}/item`, options)
             const responseJson = await response.json();
             showResponseMessage(responseJson.message);
-            getBook();
+            getItem();
         } catch(error) {
             showResponseMessage(error)
         }
-    }*/
+    }
 
-    /*const insertBook = (book) => {
-        // Membuat instance dari XMLHttpRequest
-        const xhr = new XMLHttpRequest();
-
-        //menetapkan callback jika response sukses dan error
-        xhr.onload = function() {
-            const responseJson = JSON.parse(this.responseText);
-            showResponseMessage(responseJson.message);
-            getBook();
-        }
-
-        xhr.onerror = function() {
-            showResponseMessage();
-        }
-
-        // Membuat POST request dan menetapkan target URL
-        xhr.open("POST", `${baseUrl}/add`);
-        
-        // Mementapkan properti Content-Type dan X-Auth-Token pada Header request
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.setRequestHeader("X-Auth-Token", "12345");
-
-        // Mengirimkan request dan menyisipkan JSON.stringify(book) pada body
-        xhr.send(JSON.stringify(book));
-    };*/
-
-    //dengan fetch
-    const updateBook = (book) => {
-        fetch(`${baseUrl}/item/${book.id}`, {
-             method: "PUT",
-             headers: {
+    // add transaction history
+    const insertTrx = async (trx) => {
+        try{
+            const options = {
+                method: "POST",
+                headers: {
                     "Content-Type": "application/json",
-                    //"X-Auth-Token": "12345"
-             },
-             body: JSON.stringify(book)
-         })
-         .then(response => {
-                return response.json();
-         })
-         .then(responseJson => {
-                showResponseMessage(responseJson.message);
-                getBook();
-         })
-         .catch(error => {
-                showResponseMessage(error);
-         })
-    };
+                    // "X-Auth-Token": "12345"
+                },
+                body: JSON.stringify(trx)
+            }
+     
+            const response = await fetch(`${baseUrl}/transaction`, options)
+            const responseJson = await response.json();
+            showResponseMessage(responseJson.message);
+            getTrx();
+            getItem();
+        } catch(error) {
+            showResponseMessage(error)
+        }
+    }
 
-    //dengan async/await
-    /*const updateBook = async (book) => {
+    // update item by itemId
+    const updateItem = async (itemId) => {
         try {
             const options = {
                 method: "PUT",
                 headers: {
                    "Content-Type": "application/json",
-                    "X-Auth-Token": "12345"
+                    // "X-Auth-Token": "12345"
                },
-               body: JSON.stringify(book)
+               body: JSON.stringify(itemId)
             }
         
-            const response = await fetch(`${baseUrl}/edit/${book.id}`, options);
+            const response = await fetch(`${baseUrl}/item/${itemId.id}`, options);
             const responseJson = await response.json();
         
             showResponseMessage(responseJson.message);
-            getBook();
+            getItem();
        } catch(error) {
             showResponseMessage(error);
        }
-    };*/
-    /*dengan AJAX XHR
-    const updateBook = (book) => {
-        // Membuat instance dari XMLHttpRequest
-        const xhr = new XMLHttpRequest();
- 
-        //menetapkan callback jika response sukses dan error
-        xhr.onload = function() {
-            const responseJson = JSON.parse(this.responseText);
-            showResponseMessage(responseJson.message);
-            getBook();
-        }
- 
-        xhr.onerror = function() {
-            showResponseMessage();
-        }
- 
-        // Membuat PUT request dan menetapkan target URL
-        xhr.open("PUT", `${baseUrl}/edit/${book.id}`);
-        
-        // Mementapkan properti Content-Type dan X-Auth-Token pada Header request
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.setRequestHeader("X-Auth-Token", "12345");
- 
-        // Mengirimkan request dan menyisipkan JSON.stringify(book) pada body
-        xhr.send(JSON.stringify(book));
-    };*/
-
-    //dengan fetch
-    const removeBook = (bookId) => {
-        fetch(`${baseUrl}/item/${bookId}`, {
-            method: "DELETE",
-            headers: {
-                //"X-Auth-Token": "12345"
-            }
-        })
-        .then(response => {
-            return response.json();
-        })
-        .then(responseJson => {
-            showResponseMessage(responseJson.message);
-            getBook();      
-        })
-        .catch(error => {
-            showResponseMessage(error);
-        })
     };
 
-    /*dengaan await
-    const removeBook = async (bookId) => {
+    // remove item by itemID
+    const removeItem = async (itemId) => {
         try {
             const options = {
                 method: "DELETE",
                 headers: {
-                    "X-Auth-Token": "12345"
+                    // "X-Auth-Token": "12345"
                }
             }
         
-            const response = await fetch(`${baseUrl}/delete/${bookId}`, options);
+            const response = await fetch(`${baseUrl}/item/${itemId}`, options);
             const responseJson = await response.json();
         
             showResponseMessage(responseJson.message);
-            getBook();
+            getItem();
        } catch(error) {
             showResponseMessage(error);
        }
-    };*/
-
-    /*dengan XHR
-    const removeBook = (bookId) => {
-        // Membuat instance dari XMLHttpRequest
-        const xhr = new XMLHttpRequest();
-        
-        //menetapkan callback jika response sukses dan error
-        xhr.onload = function() {
-            const responseJson = JSON.parse(this.responseText);
-            showResponseMessage(responseJson.message);
-            getBook();
-        }
-        
-        xhr.onerror = function() {
-            showResponseMessage();
-        }
-        
-        // Membuat DELETE request dan menetapkan target URL
-        xhr.open("DELETE", `${baseUrl}/delete/${bookId}`);
-        
-        // Mementapkan properti Content-Type dan X-Auth-Token pada Header request
-        xhr.setRequestHeader("X-Auth-Token", "12345");
-        
-        // Mengirimkan request
-        xhr.send();
-    };*/
-
-
-
-
-
+    };
 
     /*
         jangan ubah kode di bawah ini ya!
@@ -324,6 +148,24 @@ function main() {
                 <td>${trx.qty}</td>
                 <td>${trx.sold}</td>
                 <td>${trx.dateTrx}</td>
+                <td>${trx.itemType}</td>
+            </tr>
+            `;
+        });
+    };
+
+    const renderSeacrh = (transactions) => {
+        const listTrxElement = document.querySelector("#listTrx");
+        listTrxElement.innerHTML = "";
+
+        transactions.forEach(trx => {
+            listTrxElement.innerHTML += `
+            <tr>
+                <td>${trx.idTrx}</td>
+                <td>${trx.itemName}</td>
+                <td>${trx.qty}</td>
+                <td>${trx.sold}</td>
+                <td>${trx.trxDate}</td>
                 <td>${trx.itemType}</td>
             </tr>
             `;
@@ -353,7 +195,7 @@ function main() {
         buttons.forEach(button => {
             button.addEventListener("click", event => {
                 const itemId = event.target.id;
-                removeBook(itemId);
+                removeItem(itemId);
             })
         })
     };
@@ -369,9 +211,16 @@ function main() {
         const inputItemId = document.querySelector("#inputItemId");
         const inputItemSold = document.querySelector("#inputItemSold");
         const inputItemQty = document.querySelector("#inputItemQty");
+        const inputSearch = document.querySelector("#cari");
         const buttonSave = document.querySelector("#buttonSave");
         const buttonUpdate = document.querySelector("#buttonUpdate");
         const buttonSaveTrx = document.querySelector("#buttonSaveTrx");
+        const buttonSearch = document.querySelector("#btnCari");
+
+        buttonSearch.addEventListener("click", function () {
+            const item = inputSearch.value;
+            searchTrx(item)
+        });
 
         buttonSave.addEventListener("click", function () {
             const item = {
@@ -379,7 +228,7 @@ function main() {
                 name: inputItemName.value,
                 qty: inputItemQty.value
             };
-            insertBook(item)
+            insertItem(item)
         });
 
         buttonUpdate.addEventListener("click", function () {
@@ -389,7 +238,7 @@ function main() {
                 qty: inputItemQty.value
             };
 
-            updateBook(item)
+            updateItem(item)
         });
 
         buttonSaveTrx.addEventListener("click", function () {
